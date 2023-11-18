1af973517f37829070fc1baf5fa981a8017054ae
Michael Leighton <michael138leighton@gmail.com>
2023-11-17 20:21:48 -0500

    task 7 done

--- world.js
+++ world.js
@@ -1,13 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     const lookupButton = document.getElementById('lookup');
+    const lookupCitiesButton = document.getElementById('lookup-cities');
+    const resultDiv = document.getElementById('result');
+
     lookupButton.addEventListener('click', function() {
+        performLookup(false);
+    });
+
+    lookupCitiesButton.addEventListener('click', function() {
+        performLookup(true);
+    });
+
+    function performLookup(isCityLookup) {
         const country = document.getElementById('country').value;
-        console.log(country);
-        fetch('world.php?country='+ encodeURIComponent(country))
+        console.log(country)
+        let url = 'world.php?country=' + encodeURIComponent(country);
+        if (isCityLookup) {
+            url += '&lookup=cities';
+        }
+
+        fetch(url)
             .then(response => response.text())
             .then(data => {
-                document.getElementById('result').innerHTML = data;
+                resultDiv.innerHTML = data;
             })
             .catch(error => console.error('Error:', error));
-    });
+    }
 });
--- world.php
+++ world.php
@@ -6,30 +6,61 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-// Check if the 'country' GET variable is set and not empty
-if (isset($_GET['country']) && !empty($_GET['country'])) {
+if (isset($_GET['country'])) {
     $country = $_GET['country'];
-    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+    // Check if 'lookup' parameter exists and is set to 'cities'
+    $isCityLookup = isset($_GET['lookup']) && $_GET['lookup'] == 'cities';
+
+    if ($isCityLookup) {
+        // SQL query for cities
+        $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population 
+                                FROM cities 
+                                JOIN countries ON cities.country_code = countries.code 
+                                WHERE countries.name LIKE :country");
+    } else {
+        // SQL query for country information
+        $stmt = $conn->prepare("SELECT name, continent, independence_year, head_of_state 
+                                FROM countries 
+                                WHERE name LIKE :country");
+    }
+
+    // Bind the 'country' parameter to the statement
+    $stmt->bindValue(':country', "%$country%");
+
+    // Execute the query
+    $stmt->execute();
+
+    // Fetch all results
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // Output the results in an HTML table
+    echo "<table border='1'>";
+    if ($isCityLookup) {
+        echo "<tr><th>Name</th><th>District</th><th>Population</th></tr>";
+        foreach ($results as $row) {
+            echo "<tr>
+                    <td>" . htmlspecialchars($row['name']) . "</td>
+                    <td>" . htmlspecialchars($row['district']) . "</td>
+                    <td>" . htmlspecialchars($row['population']) . "</td>
+                  </tr>";
+        }
+    } else {
+        // Code for handling country information output
+        echo "<tr><th>Name</th><th>Continent</th><th>Independence</th>
+                <th>Head of State</th></tr>";
+        foreach($results as $row){
+            echo "<tr>
+                    <td>" . htmlspecialchars($row['name']) . "</td>
+                    <td>" . htmlspecialchars($row['continent']) . "</td>
+                    <td>" . htmlspecialchars($row['independence_year']) . "</td>
+                    <td>" . htmlspecialchars($row['head_of_state']) . "</td>
+                  </tr>";
+        }
+
+    }
+    echo "</table>";
 } else {
-    $stmt = $conn->query("SELECT * FROM countries");
+    echo "No country specified";
 }
-
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-<table>
-    <tr>
-        <th>Country Name</th>
-        <th>Continent</th>
-        <th>Independence Year</th>
-        <th>Head of State</th>
-    </tr>
-    <?php foreach ($results as $row): ?>
-        <tr>
-            <td><?= htmlspecialchars($row['name']) ?></td>
-            <td><?= htmlspecialchars($row['continent']) ?></td>
-            <td><?= htmlspecialchars($row['independence_year']) ?></td>
-            <td><?= htmlspecialchars($row['head_of_state']) ?></td>
-        </tr>
-    <?php endforeach; ?>
-</table>
